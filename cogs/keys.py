4cca349e48b280e5e38d112a66e054015c1d3e78
Mathew <101397428+Matvey4A@users.noreply.github.com>
2024-11-21 18:29:05 +0400

    Update keys.py

--- cogs/keys.py
+++ cogs/keys.py
@@ -1,6 +1,6 @@
 # Bot API keys and main variables
 
-botToken = "MTMwNDAxMzUxNTMzNzYyOTc3Nw.GDC0OY.jNuM9mOW5vFgnBkehCNRCIyXX9-SGi2Oc-ySxw"
+botToken = "**************************************************"
 serverId = 758506006778478595
 content_role_to_mention_id = 1020292340813922335
 tester_role_id = 1304468387874738206
