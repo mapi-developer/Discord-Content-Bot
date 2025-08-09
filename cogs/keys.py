7fb1d599e6e1a01f6f0552875ed2f145af9423db
Mapi <101397428+mapi-developer@users.noreply.github.com>
2025-08-09 12:39:22 +0400

    Update keys.py

--- cogs/keys.py
+++ cogs/keys.py
@@ -1,7 +1,8 @@
 # Bot API keys and main variables
 
 botToken = "**************************************************"
-serverId = 758506006778478595
-content_role_to_mention_id = 1020292340813922335
-tester_role_id = 1304468387874738206
+serverId = 000000000000
+content_role_to_mention_id = 0000000
+tester_role_id = 000000000000
 clear_old_data_time = 1 * 60 * 60
+
